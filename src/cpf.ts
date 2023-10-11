d0740ef390ea62ec243701b2c45c4c41889ca5e4
becardine <bealvescardine@gmail.com>
2023-10-11 19:48:43 -0300

    feat: first commit

---
+++ src/cpf.ts
@@ -0,0 +1,49 @@
+export default class Cpf {
+	value: string
+
+	constructor(value: string) {
+		if (!this.validate(value)) throw new Error('Invalid cpf')
+		this.value = value
+	}
+
+	FACTOR_DIGIT_1 = 10
+	FACTOR_DIGIT_2 = 11
+
+	validate(rawCpf: string) {
+		if (!rawCpf) return
+		const cpf = this.clean(rawCpf)
+		if (!this.hasMinimumLength(cpf)) return false
+		if (this.isBlocked(cpf)) return false
+		const digit1 = this.calculateDigit(cpf, this.FACTOR_DIGIT_1)
+		const digit2 = this.calculateDigit(cpf, this.FACTOR_DIGIT_2)
+		const actualDigit = this.extractActualDigit(cpf)
+		const calculatedDigit = `${digit1}${digit2}`
+		return actualDigit === calculatedDigit
+	}
+
+	clean(cpf: string) {
+		return cpf.replace(/[\.\-]*/g, '')
+	}
+
+	hasMinimumLength(cpf: string) {
+		return cpf.length === 11
+	}
+
+	isBlocked(cpf: string) {
+		const [firstDigit] = cpf
+		return [...cpf].every((digit) => digit === firstDigit)
+	}
+
+	calculateDigit(cpf: string, factor: number) {
+		let total = 0
+		for (const digit of cpf) {
+			if (factor > 1) total += parseInt(digit) * factor--
+		}
+		const rest = total % 11
+		return rest < 2 ? 0 : 11 - rest
+	}
+
+	extractActualDigit(cpf: string) {
+		return cpf.slice(9)
+	}
+}
